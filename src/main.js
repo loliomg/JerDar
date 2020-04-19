ef2f0ec787eae15de201f58698e7ff2e63de38cc
futiwolf <lf@lf112.net>
2020-04-19 11:13:36 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ new Vue({
 if (process.env.NODE_ENV === 'production') {
 	console.clear()
 	console.log(
-		"\n %c \u26a1RECOGNIZE %c https://recognize.naviki.cn %c BY%c LF112  \n\n",
+		"\n %c \u26a1OHMYGA233 %c https://github.com/loliomg/JerDar %c BY%c LF112  \n\n",
 		"color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;",
 		"background:rgba(197, 197, 197, 0.89); padding:5px 0;",
 		"color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;", "color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;"
